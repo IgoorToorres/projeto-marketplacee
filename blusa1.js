b27f8b06f120c060b6d73a10259c953decd990bf
IgoorToorres <115846114+IgoorToorres@users.noreply.github.com>
2023-06-23 16:31:52 -0300

    funcionando opcoes

--- blusa1.js
+++ blusa1.js
@@ -59,6 +59,36 @@ btnRemoverBlusa.addEventListener('click', () => {
     }
 });
 
+//blusa selecionada
+const principal = document.querySelector(".imagem_produto");
+const op1 = document.querySelector(".opcao1");
+const op2 = document.querySelector(".opcao2");
+
+function removerSelecionado(){
+    const opcoes = [op1, op2];
+    opcoes.forEach(opcao => {
+        opcao.classList.remove('selecionado');
+    });
+}
+
+op1.classList.add('selecionado');
+
+op1.addEventListener('click', () => {
+    principal.style.backgroundImage = 'url(imagens/produtos/CAMISETA\\ ASSC\\ SPIDER.webp)';
+    removerSelecionado();
+    op1.classList.add('selecionado');
+});
+
+op2.addEventListener('click', () => {
+    principal.style.backgroundImage = 'url(imagens/produtos/CAMISETA\\ ASSC\\ SPIDER\\ \\(2\\).webp)';
+    removerSelecionado();
+    op2.classList.add('selecionado');
+});
+
+
+
+
+
 
 
 
--- blusa2.js
+++ blusa2.js
@@ -59,6 +59,59 @@ btnRemoverBlusa.addEventListener('click', () => {
     }
 });
 
+//trocando as blusas quando selecionado
+const principal2 = document.querySelector(".imagem_produto.blusa2");
+const op3 = document.querySelector(".imagens_produto.opcao3");
+const op4 = document.querySelector(".opcao4");
+const op5 = document.querySelector(".opcao5");
+const op6 = document.querySelector(".opcao6");
+const cor1 = document.querySelector(".cor_produto#cor1");
+const cor2 = document.querySelector(".cor_produto#cor2");
+
+function removerSelecionado(){
+    const opcoes = [op3, op4, op5, op6];
+    opcoes.forEach(opcao => {
+        opcao.classList.remove('selecionado');
+    });
+}
+
+op3.classList.add('selecionado');
+
+op3.addEventListener('click', () => {
+    principal2.style.backgroundImage = 'url(imagens/produtos/CAMISETA\\ PALACE\\ HESH\\ MIT\\ TEE.webp)';
+    removerSelecionado();
+    op3.classList.add('selecionado');
+});
+
+op4.addEventListener('click', () => {
+    principal2.style.backgroundImage = 'url(imagens/produtos/CAMISETA\\ PALACE\\ HESH\\ MIT\\ TEE\\ \\(2\\).webp)';
+    removerSelecionado();
+    op4.classList.add('selecionado');
+});
+
+op5.addEventListener('click', () => {
+    principal2.style.backgroundImage = 'url(imagens/produtos/CAMISETA\\ PALACE\\ HESH\\ MIT\\ TEE\\ \\(3\\).webp)';
+    removerSelecionado();
+    op5.classList.add('selecionado');
+});
+
+op6.addEventListener('click', () => {
+    principal2.style.backgroundImage = 'url(imagens/produtos/CAMISETA\\ PALACE\\ HESH\\ MIT\\ TEE\\ \\(4\\).webp)';
+    removerSelecionado();
+    op6.classList.add('selecionado');
+});
+
+cor1.addEventListener('click', () => {
+    principal2.style.backgroundImage = 'url(imagens/produtos/CAMISETA\\ PALACE\\ HESH\\ MIT\\ TEE.webp)';
+});
+
+cor2.addEventListener('click', () => {
+    principal2.style.backgroundImage = 'url(imagens/produtos/CAMISETA\\ PALACE\\ HESH\\ MIT\\ TEE\\ \\(3\\).webp)';
+});
+
+
+
+
 
 
 
