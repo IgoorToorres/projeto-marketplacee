51e180312c77e187028ad75fb3c45f81c9a80f3c
IgoorToorres <115846114+IgoorToorres@users.noreply.github.com>
2023-06-26 16:28:07 -0300

    simplificando aquivo js

--- blusa1.js
+++ blusa1.js
@@ -1,31 +1,3 @@
-const abrirModal = document.querySelector("#abrir_modal");
-const fecharModal = document.querySelector("#fechar_modal");
-const fade = document.querySelector("#fade");
-const modal = document.querySelector("#modal");
-const trocarModal = document.querySelector(".trocar_modal");
-
-const abrirModal2 = document.querySelector(".abrir_modal2");
-const fehcarModal2 = document.querySelector("#fechar_modal2");
-const modal2 = document.querySelector("#modal2");
-const fade2 = document.querySelector("#fade2");
-
-
-const toggleModal = () => {
-    [modal, fade].forEach((el) => el.classList.toggle("hide"));
-};
-
-[abrirModal, fecharModal].forEach((el) => {
-    el.addEventListener("click", () => toggleModal());
-});
-
-
-const toggleModal2 = () => {
-    [modal2, fade].forEach((el) => el.classList.toggle("hide"));
-};
-
-[abrirModal2, fehcarModal2].forEach((el) => {
-    el.addEventListener("click", () => toggleModal2());
-});
 
 
 //butao de adicionar mais roupas
--- script.js
+++ script.js
@@ -16,38 +16,6 @@ function nextImage(){
 //fim efeito carrosel
 
 
-//abrindo tela modal de login
-
-const abrirModal = document.querySelector("#abrir_modal");
-const fecharModal = document.querySelector("#fechar_modal");
-const fade = document.querySelector("#fade");
-const modal = document.querySelector("#modal");
-const trocarModal = document.querySelector(".trocar_modal");
-
-const abrirModal2 = document.querySelector(".abrir_modal2");
-const fehcarModal2 = document.querySelector("#fechar_modal2");
-const modal2 = document.querySelector("#modal2");
-const fade2 = document.querySelector("#fade2");
-
-
-const toggleModal = () => {
-    [modal, fade].forEach((el) => el.classList.toggle("hide"));
-};
-
-[abrirModal, fecharModal].forEach((el) => {
-    el.addEventListener("click", () => toggleModal());
-});
-
-
-const toggleModal2 = () => {
-    [modal2, fade].forEach((el) => el.classList.toggle("hide"));
-};
-
-[abrirModal2, fehcarModal2].forEach((el) => {
-    el.addEventListener("click", () => toggleModal2());
-});
-
-
 
 
 
